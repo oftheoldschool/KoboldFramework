62a5f48565278220be6a886bc473fcd22da2549e
Andy Stanton <andystanton@users.noreply.github.com>
2024-07-12 13:12:42 +0100

    Bump minimum iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SPMKoboldFramework",
     platforms: [
-        .iOS("13.0")
+        .iOS("17.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
