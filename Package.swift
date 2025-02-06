fbd502e694fcac1813aecca4efe703821078cd44
Andy Stanton <andystanton@users.noreply.github.com>
2025-02-06 08:49:45 +0000

    SPMKoboldFramework -> KoboldFramework

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SPMKoboldFramework",
+    name: "KoboldFramework",
     platforms: [
         .iOS("17.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SPMKoboldFramework",
-            targets: ["SPMKoboldFramework"]),
+            name: "KoboldFramework",
+            targets: ["KoboldFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SPMKoboldFramework")
+            name: "KoboldFramework")
     ]
 )
