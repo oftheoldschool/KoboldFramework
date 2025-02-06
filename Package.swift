c75d3b21a8eda93049b8177163c4eb9bfd1b9717
Andy Stanton <andystanton@users.noreply.github.com>
2025-02-06 14:28:11 +0000

    split logging out

--- Package.swift
+++ Package.swift
@@ -13,11 +13,17 @@ let package = Package(
         .library(
             name: "KoboldFramework",
             targets: ["KoboldFramework"]),
+        .library(
+            name: "KoboldLogging",
+            targets: ["KoboldLogging"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KoboldFramework")
+            name: "KoboldFramework",
+            dependencies: ["KoboldLogging"]),
+        .target(
+            name: "KoboldLogging")
     ]
 )
