be2d02e67dfadb59de1e7e337e64b3e254789f61
Andy Stanton <andystanton@users.noreply.github.com>
2025-02-07 22:31:34 +0000

    Separate out KLogging

--- Package.swift
+++ Package.swift
@@ -1,29 +1,24 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KoboldFramework",
     platforms: [
-        .iOS("17.0")
+        .iOS("17.0"),
+        .macOS("14.0")
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KoboldFramework",
             targets: ["KoboldFramework"]),
-        .library(
-            name: "KoboldLogging",
-            targets: ["KoboldLogging"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/oftheoldschool/KoboldLogging.git", "0.0.1"..<"1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "KoboldFramework",
             dependencies: ["KoboldLogging"]),
-        .target(
-            name: "KoboldLogging")
     ]
 )
