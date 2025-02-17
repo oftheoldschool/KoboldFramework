2ddeb49b8e9ff88df291b6f15c12a51a6566b1c4
Andy Stanton <andystanton@users.noreply.github.com>
2025-02-17 09:12:29 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "KoboldFramework",
-    platforms: [
-        .iOS("17.0"),
-        .macOS("14.0")
-    ],
     products: [
         .library(
             name: "KoboldFramework",
